716624148807baf1ed847dbcb665cf8caead1b5e
mahmoud <mahmoudebrahim049@gmail.com>
2026-05-07 22:45:53 +0300

    update

--- واصل-Wasel/واصل-Wasel/src/lib/supabase.ts
+++ واصل-Wasel/واصل-Wasel/src/lib/supabase.ts
@@ -3,5 +3,5 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = 'https://pnhwskxbhtgzckyjkvtn.supabase.co';
 const supabaseKey = 'sb_publishable_JPHYVymHQ9iFjLigksc6Hw_3YbTiGJR';
 
-export const isSupabaseConfigured = false;
+export const isSupabaseConfigured = true;
 export const supabase = createClient(supabaseUrl, supabaseKey);
